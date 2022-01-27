179350f06d22ea1d6dda9a9d4447f4a4154ef56d
Moises Junior <m.junior@eq.ufrj.br>
2022-01-27 15:14:29 -0300

    feat: post participants

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,9 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
+import Joi from 'joi';
+import dayjs from 'dayjs'
+import 'dayjs/locale/pt-br.js'
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -8,38 +11,45 @@ const server = express();
 server.use(cors());
 server.use(json());
 
-const connectBatePapo = new MongoClient(process.env.BATEPAPO_URI);
+const mongoClient = new MongoClient(process.env.BATEPAPO_URI);
+let db;
+mongoClient.connect(() => {
+    db = mongoClient.db("salaBatePapo");
+})
 
-// server.post('/participants', async (req, res) => {
-//     const name = req.body.name
+server.post('/participants', async (req, res) => {
+    const name = req.body.name;
+    const hour = dayjs().locale('pt-br').format('HH:mm:ss')
+    const message = {
+        from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: hour
+    }
 
-//     try {
-//         await connectBatePapo.connect();
-//         const dbSala = connectBatePapo.db("salaBatePapo");
-//         const participantsCollection = dbSala.collection("participants");
-//         const participants = await participantsCollection.find({});
-//         participants.insert({name, lastStatus: Date.now()});
-//         res.sendStatus(201);
-//         connectBatePapo.close();
-//     } catch  {
-//         res.sendStatus(500);
-//         console.log('Erro!');
-//         connectBatePapo.close();
-//     }
-// });
+    try {
+        Joi.attempt(name, Joi.string());
+        try {
+            await db.collection("participants").insertOne({name, lastStatus: Date.now()});
+            await db.collection("messages").insertOne(message)
+            res.sendStatus(201);
+            db.close();
+        } catch  {
+            res.sendStatus(500);
+            console.log('Erro!');
+            db.close();
+        }
+    } catch {
+        res.sendStatus(422);
+    }
+});
 
 server.get('/participants', async (req, res) => {
     try {
-        await connectBatePapo.connect();
-        const dbSala = connectBatePapo.db("salaBatePapo");
-        const participantsCollection = dbSala.collection("participants");
-        const participants = await participantsCollection.find({}).toArray();
+        const participants = await db.collection("participants").find({}).toArray();
         res.send(participants);
-        connectBatePapo.close();
+        db.close();
     } catch {
         res.sendStatus(500);
         console.log('Erro!');
-        connectBatePapo.close();
+        db.close();
     }
 });
 
