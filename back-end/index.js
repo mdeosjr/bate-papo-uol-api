b79a6f86fea90bfa287ff3f889a0f92c7320251d
Moises Junior <m.junior@eq.ufrj.br>
2022-01-26 23:41:19 -0300

    feat: get participants

--- back-end/index.js
+++ back-end/index.js
@@ -8,6 +8,39 @@ const server = express();
 server.use(cors());
 server.use(json());
 
-const MongoClient = new MongoClient(process.env.BATEPAPO_URI);
+const connectBatePapo = new MongoClient(process.env.BATEPAPO_URI);
+
+// server.post('/participants', async (req, res) => {
+//     const name = req.body.name
+
+//     try {
+//         await connectBatePapo.connect();
+//         const dbSala = connectBatePapo.db("salaBatePapo");
+//         const participantsCollection = dbSala.collection("participants");
+//         const participants = await participantsCollection.find({});
+//         participants.insert({name, lastStatus: Date.now()});
+//         res.sendStatus(201);
+//         connectBatePapo.close();
+//     } catch  {
+//         res.sendStatus(500);
+//         console.log('Erro!');
+//         connectBatePapo.close();
+//     }
+// });
+
+server.get('/participants', async (req, res) => {
+    try {
+        await connectBatePapo.connect();
+        const dbSala = connectBatePapo.db("salaBatePapo");
+        const participantsCollection = dbSala.collection("participants");
+        const participants = await participantsCollection.find({}).toArray();
+        res.send(participants);
+        connectBatePapo.close();
+    } catch {
+        res.sendStatus(500);
+        console.log('Erro!');
+        connectBatePapo.close();
+    }
+});
 
 server.listen(4000);
