1a002938096ad2b32074ad94ad6084c90828b35d
Moises Junior <m.junior@eq.ufrj.br>
2022-01-29 01:08:52 -0300

    refactor: add mongoConnect function

--- back-end/index.js
+++ back-end/index.js
@@ -3,46 +3,65 @@ import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import Joi from 'joi';
 import dayjs from 'dayjs'
-import 'dayjs/locale/pt-br.js'
 import dotenv from 'dotenv';
+import res from 'express/lib/response';
 dotenv.config();
 
 const server = express();
 server.use(cors());
 server.use(json());
 
-const mongoClient = new MongoClient(process.env.BATEPAPO_URI);
-let db;
-mongoClient.connect(() => {
-    db = mongoClient.db("salaBatePapo")
-})
-const hour = dayjs().locale('pt-br').format('HH:mm:ss')
+async function mongoConnect() {
+    try {
+        const mongoClient = new MongoClient(process.env.BATEPAPO_URI);
+        await mongoClient.connect();
+        const db = mongoClient.db("salaBatePapo")
+        return { mongoClient, db };
+    } catch (error) {
+        console.error(error)
+    }
+};
 
 setInterval(async () => {
     try {
+        const { mongoClient, db } = await mongoConnect();
         const participants = await db.collection("participants").find({}).toArray();
-        await db.collection("messages").find({});
         const lastStatusNow = Date.now();
 
         for (const participant of participants) {
             if (participant.lastStatus < lastStatusNow - 10000) {
-                await db.collection("participants").deleteMany({ lastStatus: participant.lastStatus });
-                await db.collection("messages").insertMany({ from: participant.name, to: 'Todos', text: 'sai da sala...', type: 'status', time: hour })
+                await db.collection("participants").deleteOne({ lastStatus: participant.lastStatus });
+                await db.collection("messages").insertOne(
+                    {
+                        from: participant.name, 
+                        to: 'Todos', 
+                        text: 'sai da sala...', 
+                        type: 'status', 
+                        time: dayjs().format('HH:mm:ss')
+                    }
+                )
             }
         }
+        mongoClient.close();
     } catch (error) {
-        console.log(error);
+        const { mongoClient } = await mongoConnect();
+        res.sendStatus(500);
+        mongoClient.close();
     }
-   
-}, 15000)
+}, 15000);
 
 server.post('/participants', async (req, res) => {
+    const { mongoClient, db } = await mongoConnect();
+    const participant = await db.collection("participants").findOne({ name: name });
     const name = req.body.name;
     const message = {
-        from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: hour
+        from: name, 
+        to: 'Todos', 
+        text: 'entra na sala...', 
+        type: 'status', 
+        time: dayjs().format('HH:mm:ss')
     };
-    const participant = await db.collection("participants").findOne({ name: name });
-    const validation = Joi.attempt(name, Joi.string().required())
+    const validation = Joi.attempt(name, Joi.string().required());
     
     if (participant) {
         res.sendStatus(409);
@@ -58,40 +77,43 @@ server.post('/participants', async (req, res) => {
         await db.collection("participants").insertOne({name, lastStatus: Date.now()});
         await db.collection("messages").insertOne(message)
         res.sendStatus(201);
-        db.close();
+        mongoClient.close();
     } catch  {
         res.sendStatus(500);
-        console.log('Erro!');
-        db.close();
+        mongoClient.close();
     }
     
 });
 
 server.get('/participants', async (req, res) => {
+    const { mongoClient, db } = await mongoConnect();
     try {
         const participants = await db.collection("participants").find({}).toArray();
         res.send(participants);
-        db.close();
+        mongoClient.close();
     } catch {
         res.sendStatus(500);
         console.log('Erro!');
-        db.close();
+        mongoClient.close();
     }
 });
 
 server.post('/messages', async (req, res) => {
+    const { mongoClient, db } = await mongoConnect();
     const user = req.headers.user;
     const textUser = req.body;
-    const message = {...textUser, from: user, time: hour};
     const participant = await db.collection("participants").findOne({ name: user });
+    const message = {...textUser, from: user, time: dayjs().format('HH:mm:ss')};
 
     const messageSchema = Joi.object({
         to: Joi.string().required(),
         text: Joi.string().required(),
         type: Joi.string().required().valid('message', 'private_message'),
+        from: any().required(),
+        time:any().required()
     })
 
-    const validation = messageSchema.validate(textUser, { abortEarly: false });
+    const validation = messageSchema.validate(textUser);
     if (validation.error || !participant) {
         res.status(422).send(validation.error);
         return
@@ -100,33 +122,37 @@ server.post('/messages', async (req, res) => {
     try {
         await db.collection("messages").insertOne(message)
         res.sendStatus(201);
-        db.close();
+        mongoClient.close();
     } catch {
         res.sendStatus(422);
-        db.close();
+        mongoClient.close();
     }
 })
 
 server.get('/messages', async (req, res) => {
-    try {
-        const limit = parseInt(req.query.limit)
-        const messages = await db.collection("messages").find({}).toArray();
-    
-        if (!limit) {
+    const { mongoClient, db } = await mongoConnect();
+    const limit = parseInt(req.query.limit)
+
+    if (!limit) {
             res.send(messages);
-            db.close();
+            mongoClient.close();
+            return;
         }
 
-        const endIndex = messages.length - 1;
-        const startIndex = endIndex - limit;
-        const limitedMessages = [...messages].slice(startIndex, endIndex);
-        res.send(limitedMessages);
-    } catch (error) {
+    try {
+        const messages = await db.collection("messages").find({}).toArray();
+    
+        res.send(messages.slice(-limit));
+        mongoClient.close();
+
+    } catch {
         res.sendStatus(500)
+        mongoClient.close();
     }
 })
 
 server.post('/status', async (req, res) => {
+    const { mongoClient, db } = await mongoConnect();
     const user = req.headers.user;
     const participant = await db.collection("participants").findOne({ name: user });
         
@@ -137,11 +163,17 @@ server.post('/status', async (req, res) => {
 
     try {
         await db.collection("participants").updateOne({ name: user }, { $set: { lastStatus: Date.now() }});
-        res.sendStatus(200)
+        res.status(200).send(participant);
+        mongoClient.close();
     } catch {
         res.sendStatus(500)
+        mongoClient.close();
     }
 })
+
+// server.delete('/messages/:id', async (req, res) => {
+
+// })
 
 server.listen(5000);
 
